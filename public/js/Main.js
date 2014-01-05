05c9ad78a423cc53319f806eee7e3fa54bed2f45
Viper Bailey <jinxidoru@gmail.com>
2014-01-04 22:23:04 -0700

    store the character in local storage

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,18 +1,33 @@
+"use strict";
 define(function(require) {
+  var storageId = '13thAgeChar';
+
   return {
     controller: function($scope) {
-      $scope.name = "Mike";
-      $scope.char = {
-        level: 2,
-        str: 18,
-        con: 12,
-        dex: 10,
-        int: 8,
-        cha: 14,
-        wis: 16,
-        cls: 'Cleric'
-      };
 
+      // load the character from local storage
+      try {
+        $scope.char = JSON.parse(localStorage[storageId]);
+      } catch (e) {}
+
+      // define a default character
+      if ( !$scope.char ) {
+        $scope.char = {
+          level: 1,
+          str: 18,
+          con: 12,
+          dex: 10,
+          int: 8,
+          cha: 14,
+          wis: 16,
+          cls: 'Cleric'
+        };
+      }
+
+      // save the character definition
+      $scope.$watch('char',function(val) {
+        localStorage[storageId] = JSON.stringify(val);
+      },true);
     }
   }
 });
